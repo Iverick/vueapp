7589f69520c3b8d05565624b35c0e87e75f56fa5
Pawel Goloblia <iverick091@gmail.com>
2021-01-19 17:04:50 +0200

    Installed and added to the app a Vuex store.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,9 +3,10 @@ import Vue from 'vue';
 import App from '../components/App.vue';
 import ListingPage from '../components/ListingPage.vue';
 import router from './router';
+import store from './store';
 
 var app = new Vue({
   el: '#app',
 
-  render: h => h(App), router
+  render: h => h(App), router, store
 });
